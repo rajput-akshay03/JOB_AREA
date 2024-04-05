255fa36d7e6c8ff24f229a207f310c2a71201def
rajput-akshay03 <akshayrajput152003@gmail.com>
2024-04-05 18:57:32 +0530

    change

--- Backend/middlewares/auth.js
+++ Backend/middlewares/auth.js
@@ -12,5 +12,6 @@ const isAuthorized= catchAsyncError(async(req,res,next)=>{
       const decoded=jwt.verify(token,process.env.JWT_SECRET_KEY);
       req.user= await User.findById(decoded.id);
       next();
+      console.log("");
 })
 module.exports={isAuthorized};
